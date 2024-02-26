4fc8bab32fca6f799956b63250faa04311e862e9
James Lee <68933375+gengucan@users.noreply.github.com>
2024-02-25 16:50:47 -0800

    Added constants, fixed Gui

--- Constants.hs
+++ Constants.hs
@@ -1,105 +1,70 @@
 module Constants where
 
--- import Graphics.Gloss
--- import Graphics.Gloss.Data.Color
+import Graphics.Gloss
+import Graphics.Gloss.Data.Color
 
 -- -- Define the screen width of the game window
--- screenWidth :: Int 
--- screenWidth = 800
+screenWidth :: Int 
+screenWidth = 800
 
 -- -- Define the screen height of the game window
--- screenHeight :: Int
--- screenHeight = 550
+screenHeight :: Int
+screenHeight = 550
 
 -- -- Define the background color of the game window screen 
--- screenBackgroundColor :: Color
--- screenBackgroundColor = makeColorI 250 240 230 0
+screenBackgroundColor :: Color
+screenBackgroundColor = makeColorI 250 240 230 0
 
 -- Define the grid size 
 gridSize :: Int
 gridSize = 10
 
 -- -- Define the grid cell width
--- cellWidth :: Float
--- cellWidth = 50
+cellWidth :: Float
+cellWidth = 50
 
 -- -- Define solid-color rectangles
--- filledSquare :: Picture
--- filledSquare = rectangleSolid cellWidth cellWidth
+filledSquare :: Picture
+filledSquare = rectangleSolid cellWidth cellWidth
 
 -- -- Define outline-only rectangles
--- outlinedSquare :: Picture 
--- outlinedSquare = rectangleWire cellWidth cellWidth
+outlinedSquare :: Picture 
+outlinedSquare = rectangleWire cellWidth cellWidth
 
 -- -- RollButton
--- rollButtonWidth :: Int
--- rollButtonWidth = 200
+rollButtonWidth :: Int
+rollButtonWidth = 200
 
--- rollButtonHeight :: Int
--- rollButtonHeight = 100
+rollButtonHeight :: Int
+rollButtonHeight = 100
 
--- rollButtonLeft :: Int
--- rollButtonLeft = -350
+rollButtonLeft :: Int
+rollButtonLeft = -350
 
--- rollButtonRight :: Int
--- rollButtonRight = rollButtonLeft + rollButtonWidth
+rollButtonRight :: Int
+rollButtonRight = rollButtonLeft + rollButtonWidth
 
--- rollButtonBottom :: Int
--- rollButtonBottom = 100
+rollButtonBottom :: Int
+rollButtonBottom = 100
 
--- rollButtonTop :: Int
--- rollButtonTop = rollButtonBottom + rollButtonHeight
+rollButtonTop :: Int
+rollButtonTop = rollButtonBottom + rollButtonHeight
 
 -- -- The text displayed in the roll button
--- rollButtonText :: String
--- rollButtonText = "Press to Roll"
-
--- --ResetButton
--- resetButtonWidth :: Int
--- resetButtonWidth = 100
-
--- resetButtonHeight :: Int
--- resetButtonHeight = 50
-
--- resetButtonLeft :: Int
--- resetButtonLeft = -300
-
--- resetButtonRight :: Int
--- resetButtonRight = resetButtonLeft + resetButtonWidth
-
--- resetButtonBottom :: Int
--- resetButtonBottom = 15
-
--- resetButtonTop :: Int
--- resetButtonTop = resetButtonBottom + resetButtonHeight
-
--- -- The text displayed in the reset button
--- resetButtonText :: String
--- resetButtonText = "Reset"
-
--- -- The color of the reset button
--- resetButtonColor :: Color
--- resetButtonColor = light(light (dim red))
-
--- -- Color representing ladders on the board
--- laddersColor :: Color
--- laddersColor = dark(dark yellow)
-
--- -- Color representing snakes on the board
--- snakesColor :: Color
--- snakesColor = dark red
+rollButtonText :: String
+rollButtonText = "Press to Roll"
 
 -- -- Color representing player one
--- playerOneColor :: Color
--- playerOneColor = dark green
+playerOneColor :: Color
+playerOneColor = dark red
 
 -- -- Color representing player two
--- playerTwoColor :: Color
--- playerTwoColor = dark cyan
+playerTwoColor :: Color
+playerTwoColor = dark cyan
 
 -- -- Define a light black color for the grid
--- lightBlack :: Color
--- lightBlack = light(light black)
+lightBlack :: Color
+lightBlack = light(light black)
 
 -- Indexes for 2d array for the path of Player 1
 player1Path :: [(Int,Int)]
--- Gui.hs
+++ Gui.hs
@@ -2,7 +2,8 @@ module Gui where
 
 --import qualified Data.Map as Map
 import Graphics.Gloss
-import Constants
+import qualified Constants as C
+import GameData
 
 -- Determine if the game has just started
 -- Return true if yes, false otherwise
@@ -15,8 +16,8 @@ isGameStart state = p1 == [17,17] && p2 == [84,84]
 -- Function to render text in a specific cell of the grid
 renderTextInCell :: Int -> Int -> String -> Color -> Picture
 renderTextInCell x y txt clr =
-    translate (fromIntegral x * cellWidth) (fromIntegral y * cellWidth) $
-    translate (-cellWidth / 2) (-cellWidth / 2) $ -- Adjust for centering the text
+    translate (fromIntegral x * C.cellWidth) (fromIntegral y * C.cellWidth) $
+    translate (-C.cellWidth / 2) (-C.cellWidth / 2) $ -- Adjust for centering the text
     scale 0.1 0.1 $ -- Adjust the text size as needed
     color clr $ -- Set text color - need to set to match the player
     text txt
@@ -24,22 +25,26 @@ renderTextInCell x y txt clr =
 -- take in cell; needs to account for overlap? - do we trigger the overlap check here?
 updateCellColor :: ([Int],[Int]) -> Int -> Int -> Picture 
 updateCellColor cell x y
-  | cell !! 1 !! 1 != 0 = color playerOneColor filledSquare
-    if cell !! 1 !! 1 == 1 then renderTextInCell x y "1" playerOneColor else renderTextInCell x y "2" playerOneColor
-  | cell !! 2 !! 1 != 0 = color playerTwoColor filledSquare
-    if cell !! 2 !! 1 == 1 then renderTextInCell x y "1" playerTwoColor else renderTextInCell x y "2" playerTwoColor
-  | otherwise = color lightBlack outlinedSquare
+  | cell !! 1 !! 1 /= 0 = 
+    if (cell !! 1 !! 1) == 1 
+      then color C.playerOneColor C.filledSquare 
+      else renderTextInCell x y "2" C.playerOneColor
+  | cell !! 2 !! 1 /= 0 = 
+    if (cell !! 2 !! 1) == 1 
+      then color C.playerTwoColor C.filledSquare 
+      else renderTextInCell x y "2" C.playerTwoColor
+  | otherwise = color C.lightBlack C.outlinedSquare
 
 -- Define the game board grid
 gridPicture :: GameState -> PlayerState -> Picture
 gridPicture gameState playerState = pictures
   [ -- Draw the grid
     translate (-100) (-250 + 25) $ pictures
-      [ translate (fromIntegral x * cellWidth) (fromIntegral y * cellWidth) $
+      [ translate (fromIntegral x * C.cellWidth) (fromIntegral y * C.cellWidth) $
             updateCellColor (currentState !! y !! x) y x
-          , translate ((-cellWidth/2) + 50 * fromIntegral x) (10 + 50 * fromIntegral y) $ scale 0.1 0.1
+          , translate ((-C.cellWidth/2) + 50 * fromIntegral x) (10 + 50 * fromIntegral y) $ scale 0.1 0.1
       ]
-        | x <- [0..gridSize-1], y <- [0..gridSize-1]
+        | x <- [0..C.gridSize-1], y <- [0..C.gridSize-1]
   ]
   where
       currentState = grid gameState
@@ -47,14 +52,14 @@ gridPicture gameState playerState = pictures
 -- Adjust button color to match current player 
 changeButtonColor :: PlayerState -> Color
 changeButtonColor playerState
-  | turn playerState  == 1 = playerOneColor
-  | otherwise = playerTwoColor
+  | turn playerState  == 1 = C.playerOneColor
+  | otherwise = C.playerTwoColor
 
 -- Define the roll button picture
 rollButtonPicture :: PlayerState -> Picture
 rollButtonPicture state = Pictures [
-    Translate (-250) 150 $ color (changeButtonColor state) $ rectangleSolid  (fromIntegral rollButtonWidth) (fromIntegral rollButtonHeight),
-    Translate (-328) 138 $ Scale 0.18 0.18 $ Text rollButtonText
+    Translate (-250) 150 $ color (changeButtonColor state) $ rectangleSolid  (fromIntegral C.rollButtonWidth) (fromIntegral C.rollButtonHeight),
+    Translate (-328) 138 $ Scale 0.18 0.18 $ Text C.rollButtonText
     ]
 
 -- Define the "Number Rolled" screen text
@@ -66,8 +71,8 @@ rollResultText = Translate (-340) (-150) $ Scale 0.2 0.2 $ Text youRolled
 changeRollResultBoxOutline :: PlayerState -> Color
 changeRollResultBoxOutline playerState
   | isGameStart playerState = white
-  | turn playerState == 1 = playerTwoColor
-  | otherwise = playerOneColor
+  | turn playerState == 1 = C.playerTwoColor
+  | otherwise = C.playerOneColor
 
 -- Define the box representing the rolled number
 rollResultBox :: PlayerState -> Picture
@@ -86,8 +91,8 @@ isGameOver gameState = winner == 1 || winner == 2
 -- Render winning screen with name and color of winner
 renderWinningScreen :: GameState -> Picture
 renderWinningScreen state
-  | winner == 1 = color playerOneColor $ rectangleSolid (fromIntegral screenWidth) (fromIntegral screenHeight)
-  | otherwise = color playerTwoColor $ rectangleSolid (fromIntegral screenWidth) (fromIntegral screenHeight)
+  | winner == 1 = color C.playerOneColor $ rectangleSolid (fromIntegral C.screenWidth) (fromIntegral C.screenHeight)
+  | otherwise = color C.playerTwoColor $ rectangleSolid (fromIntegral C.screenWidth) (fromIntegral C.screenHeight)
   where
   winner = gameOver state
 
